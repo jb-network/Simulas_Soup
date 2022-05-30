b8313eea34d6281fa1e6c611db4d149cee7b2143
jb-network <85036487+jb-network@users.noreply.github.com>
2022-05-30 10:07:49 -0500

    Add project files.

---
+++ Simulas_Soup/Program.cs
@@ -0,0 +1,19 @@
+﻿// This is challenge work for the "C# Players Guide"
+// Level 17 Simula's Soup Challenge
+// The Goal is to use enumerations and tuples to represent soup
+// The soup has a total of three variations that a user can select from:
+// 1) Type: Soup, stew, gumbo
+// 2) Main ingredient: mushrooms, chicken, carrots, or potatoes
+// 3) Seasoning: spicy, salty, sweet
+
+
+
+
+
+// Enumerations
+enum FoodType { soup, stew, gumbo }
+enum MainIngredient { mushrooms, chicken, carrots, potatoes }
+enum SeasonType { spicy, salty, sweet }
+
+
+
